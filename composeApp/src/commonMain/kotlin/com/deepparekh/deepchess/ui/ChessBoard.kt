771ebc4f72ff7c742bc589ce8badc41d7f038cdd
deepjparekh <deepjparekh@gmail.com>
2024-09-22 21:11:03 -0400

    wip

--- composeApp/src/commonMain/kotlin/com/deepparekh/deepchess/ui/ChessBoard.kt
+++ composeApp/src/commonMain/kotlin/com/deepparekh/deepchess/ui/ChessBoard.kt
@@ -2,11 +2,17 @@ package com.deepparekh.deepchess.ui
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,12 +21,13 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ChessBoard(modifier: Modifier = Modifier) {
+    BoxWithConstraints {  }
     Box(
-        modifier = modifier.size(400.dp)
+        modifier = Modifier.fillMaxSize()
     ) {
-        Row {
+        Row(modifier = Modifier.fillMaxWidth().wrapContentHeight()) {
             for (file in 8 downTo 1) {
-                Column(modifier = Modifier.width(50.dp)) {
+                Column(modifier = Modifier.weight(1f)) {
                     for (rank in 1..8) {
                         val isDark = (rank % 2) == (file % 2)
                         println("Ranks = $rank, files = $file, isDark = $isDark")
@@ -34,7 +41,7 @@ fun ChessBoard(modifier: Modifier = Modifier) {
 
 @Composable
 fun Square(isDarkSquare: Boolean) {
-    Canvas(modifier = Modifier.size(50.dp)) {
+    Canvas(modifier = Modifier.fillMaxSize()) {
         val squareColor = if (isDarkSquare) Color.DarkGray else Color.LightGray
         drawRect(
             color = squareColor
